192bd5ce2792a4bd8678d6ae20c2eb6b5c7216a2
v.funtikov <v.funtikov@rendall.im>
2021-05-09 21:39:00 +0300

    fix: WithTran missing in repo

--- examples/example3/iuserrepo/irepo.go
+++ examples/example3/iuserrepo/irepo.go
@@ -8,6 +8,8 @@ import (
 
 // UserRepo defines repository interface for domain model domain.User.
 type UserRepo interface {
+	WithTran(ctx context.Context, f func(ctx context.Context) error) error
+
 	Get(ctx context.Context, id uuid.UUID) (*domain.User, error)
 	GetForUpdate(ctx context.Context, id uuid.UUID) (*domain.User, error)
 	Delete(ctx context.Context, id uuid.UUID) error
